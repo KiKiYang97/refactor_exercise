c633bb5edf2ab6a3aa4812f3f878bdbacbf3ec24
KiKiYang97 <dunka97@163.com>
2020-08-27 15:06:54 +0800

    [Kiki]refactor : delete the comment

--- src/statement.js
+++ src/statement.js
@@ -41,11 +41,10 @@ function statement (invoice, plays) {
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = calculateAmount(play, perf);
-    // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
     if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    //print line for this order
+
     performances_print += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
   }
